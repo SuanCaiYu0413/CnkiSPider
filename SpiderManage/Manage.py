69f25762c46c31c59cc486e7179eb5bcb3b17974
suancaiyu <2011003040@qq.com>
2017-08-30 11:49:05 +0800

    最长子串不重复字符

--- SpiderManage/Manage.py
+++ SpiderManage/Manage.py
@@ -1,2 +1,19 @@
 # -*- coding:utf-8 -*-
-from MyCnki import CnkiSpider
+from MyCnki.CnkiSpider import Spider
+import socket
+
+def main():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost',8001))
+    sock.listen(5)
+
+    while True:
+        connection, address = sock.accept()
+        print connection
+        buf = connection.recv(1024)
+        print buf
+        connection.close()
+
+if __name__ == '__main__':
+    main()
